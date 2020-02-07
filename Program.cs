46b57c62d21fb067f2c9da83725f9ded1e21f84d
Sahib Arora <sahibarora1997@gmail.com>
2020-02-06 20:56:10 -0500

    Set-up

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Home_Sweet_Home
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
