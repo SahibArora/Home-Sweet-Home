eb4def4ddc96f87def0ec377f05faaa0cc97c605
Sahib Arora <sahibarora1997@gmail.com>
2020-02-08 02:30:42 -0500

    Structure set-up

--- SQLClass.cs
+++ SQLClass.cs
@@ -14,15 +14,24 @@ namespace Home_Sweet_Home
             SqlConnection cnn = new SqlConnection(connectionString);
             try
             {
-                cnn.Open();
-                Console.WriteLine("Connection setted-up!");
+
+                // Layout Design for the tables in the constructor, below commands are working!!
+                // All data is being saved to Azure..
+                // SQL Server management is being used to query data and manage tables..
+                // Functions regarding various functionalities will go in the this class!!
+
+                /*cnn.Open();
+                SqlCommand command;
+                command = new SqlCommand("CREATE TABLE Users (Name varchar,email varchar,password varchar, gender char)", cnn);
+                command.ExecuteNonQuery();
+                command.Dispose();
+                Console.WriteLine("User table created!");*/
             }
             catch (Exception e)
             {
-                Console.WriteLine("Can't Connect to the Database " + e);
+                Console.WriteLine("Error Message! ", e);
             }
-            finally
-            {
+            finally {
                 cnn.Close();
             }
         }
